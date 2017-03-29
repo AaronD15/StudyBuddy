0d7b915a4182b0fa432535f0f135f446e6d37f75
shawnc96 <shawn.a.c96@hotmail.com>
2017-03-29 16:58:45 -0400

    Added MapsActivity which is called when the Places button in TagFragment2 is clicked. Also changed the Manifest and gradle.

---
+++ app/src/main/java/edu/usf/devices/mobile/studybuddy/UpdateProfile.java
@@ -0,0 +1,15 @@
+package edu.usf.devices.mobile.studybuddy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class UpdateProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_profile);
+
+
+    }
+}
