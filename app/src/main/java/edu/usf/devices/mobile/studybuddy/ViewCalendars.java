21789e31c470d59825da26ad902b58921d833852
shawnc96 <shawn.a.c96@hotmail.com>
2017-04-05 18:19:25 -0400

    Reading last 5 events in each calendar

--- app/src/main/java/edu/usf/devices/mobile/studybuddy/ViewCalendars.java
+++ app/src/main/java/edu/usf/devices/mobile/studybuddy/ViewCalendars.java
@@ -3,20 +3,20 @@ package edu.usf.devices.mobile.studybuddy;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.icu.text.SimpleDateFormat;
-import android.icu.util.Calendar;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class ViewCalendars extends FragmentActivity {
 
-    public final int MY_CAL_REQ=1;
+    public final int MY_CAL_REQ = 1;
     // The indices for the projection array above.
     private static final int PROJECTION_ID_INDEX = 0;
     private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
@@ -27,164 +27,121 @@ public class ViewCalendars extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_calendars);
-
-        getDataFromCalendarTable();
-
-    }
-
-    public void getDataFromCalendarTable() {
-        // Run query to get default calendar
-        //check calendar permissions
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CALENDAR}, MY_CAL_REQ);
         }
-        Cursor cur ;
-        RelativeLayout layout = (RelativeLayout) (findViewById(R.id.activity_view_calendars));
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 
-        Uri l_eventUri = Uri.parse("content://calendar/events");
-        cur= getContentResolver().query(Uri.parse("content://com.android.calendar/calendars"),
-                new String[]{"_id","calendar_displayName"},null,null,null);
+        getCalendars();
+
+    }
+
+    class MyCalendar {
+        public String name;
+        public String id;
+
+        public MyCalendar(String _name, String _id) {
+            name = _name;
+            id = _id;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    private MyCalendar m_calendars[];
+    private String m_selectedCalendarId = "0";
+    StringBuilder l_displayText = new StringBuilder();
 
 
+    private void getCalendars() {
 
-        if (cur != null && cur.getCount() > 0)
-        {
-            cur.moveToFirst();
+        String[] l_projection = new String[]{"_id", "calendar_displayName"};
+        Uri l_calendars;
+        if (Build.VERSION.SDK_INT >= 8) {
+            l_calendars = Uri.parse("content://com.android.calendar/calendars");
+        } else {
+            l_calendars = Uri.parse("content://calendar/calendars");
+        }
+        Cursor l_managedCursor = this.getContentResolver().query(l_calendars, l_projection, null, null, null);    //all calendars
+        //Cursor l_managedCursor = this.managedQuery(l_calendars, l_projection, "selected=1", null, null);   //active calendars
+        if (l_managedCursor != null) {
+            if (l_managedCursor.moveToFirst()) {
+                m_calendars = new MyCalendar[l_managedCursor.getCount()];
+                String l_calName;
+                String l_calId;
+                int l_cnt = 0;
+                int l_nameCol = l_managedCursor.getColumnIndex(l_projection[1]);
+                int l_idCol = l_managedCursor.getColumnIndex(l_projection[0]);
+                do {
+                    l_calName = l_managedCursor.getString(l_nameCol);
+                    l_calId = l_managedCursor.getString(l_idCol);
+                    m_calendars[l_cnt] = new MyCalendar(l_calName, l_calId);
+                    ++l_cnt;
+                    m_selectedCalendarId = l_calId;
+                    getEvents();
+                } while (l_managedCursor.moveToNext());
+                RelativeLayout layout = (RelativeLayout) (findViewById(R.id.activity_view_calendars));
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 
-            int calID = 0;
+               /* TextView tv1 = new TextView(this);
+                tv1.setText(l_calName);
+                layout.addView(tv1, params);*/
 
-            // Get the field values
-            calID = cur.getInt(0);
-            Toast.makeText(getApplicationContext(), "ID: " + calID
-                    , Toast.LENGTH_SHORT).show();
-            String calId = "Calendar: " + calID;
-            TextView tv1 = new TextView(this);
-            tv1.setText(calId);
-            layout.addView(tv1,params);
 
-            String[] l_projection = new String[]{"title", "dtstart", "dtend"};
-            Cursor l_managedCursor = getContentResolver().query(l_eventUri, l_projection, "calendar_id=" , null, "dtstart DESC, dtend DESC");
-            StringBuilder l_displayText = new StringBuilder();
+            }
+            l_managedCursor.close();
+        }
 
-            if (l_managedCursor != null && l_managedCursor.getCount() > 0)
-            {
-                l_managedCursor.moveToFirst();
+    }
+
+    private void getEvents() {
+        Uri l_eventUri;
+        if (Build.VERSION.SDK_INT >= 8) {
+            l_eventUri = Uri.parse("content://com.android.calendar/events");
+        } else {
+            l_eventUri = Uri.parse("content://calendar/events");
+        }
+        String[] l_projection = new String[]{"title", "dtstart", "dtend"};
+        Cursor l_managedCursor = this.getContentResolver().query(l_eventUri, l_projection, "_id=" + m_selectedCalendarId, null, "dtstart DESC, dtend DESC");
+        //Cursor l_managedCursor = this.managedQuery(l_eventUri, l_projection, null, null, null);
+        if (l_managedCursor != null) {
+
+            if (l_managedCursor.moveToFirst()) {
+
                 int l_cnt = 0;
                 String l_title;
                 String l_begin;
                 String l_end;
-
+                //StringBuilder l_displayText = new StringBuilder();
                 int l_colTitle = l_managedCursor.getColumnIndex(l_projection[0]);
                 int l_colBegin = l_managedCursor.getColumnIndex(l_projection[1]);
-                int l_colEnd = l_managedCursor.getColumnIndex(l_projection[1]);
+                int l_colEnd = l_managedCursor.getColumnIndex(l_projection[2]);
                 do {
                     l_title = l_managedCursor.getString(l_colTitle);
                     l_begin = getDateTimeStr(l_managedCursor.getString(l_colBegin));
                     l_end = getDateTimeStr(l_managedCursor.getString(l_colEnd));
-                    l_displayText.append(l_title + "\n" + l_begin + "\n" + l_end + "\n----------------\n");
+                    l_displayText.append(l_title + ": " + l_begin + " to " + l_end + "\n");
                     ++l_cnt;
-                } while (l_managedCursor.moveToNext() && l_cnt < 3);
+                } while (l_managedCursor.moveToNext() && l_cnt < 5);
 
-                TextView tv2 = new TextView(this);
-                tv2.setText(l_displayText.toString());
-                layout.addView(tv2,params);
-                l_managedCursor.close();
-
-            }
-
-
-
-
-            //displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
-
-            cur.close();
-
-
-
-
-            // Do something with the values...
-
-
-            /*TextView tv1 = new TextView(this);
-            tv1.setText(calId);
-            layout.addView(tv1);
-
-            params.addRule(RelativeLayout.BELOW, tv1.getId());*/
-
-
-        }
-
-        /*boolean val = cur.moveToFirst();
-
-        if(val){
-            int pos = cur.getPosition();
-
-
-            if(cur.isNull(0)){
-                Toast.makeText(getApplicationContext(), "Null: " + cur.isNull(0)
-                        , Toast.LENGTH_SHORT).show();
-                return;
-            }
-            else {
-                int calID = 0;
-                //String displayName = null;
-
-                // Get the field values
-                //calID = cur.getInt(0);
-                //displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
-
-
-                cur.close();
-
-                // Do something with the values...
+                RelativeLayout layout = (RelativeLayout) (findViewById(R.id.activity_view_calendars));
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 
                 TextView tv1 = new TextView(this);
-                tv1.setText(calID);
-                layout.addView(tv1);
-
+                tv1.setText(l_displayText);
+                layout.addView(tv1, params);
+                //  m_text_event.setText(l_displayText.toString());
             }
-        }
-
-            //int CalID = cur.getInt(0);
-        /*ContentResolver cr = getContentResolver();
-
-        Uri uri = CalendarContract.Calendars.CONTENT_URI;
-        String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
-                + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?) AND ("
-                + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
-        String[] selectionArgs = new String[] {"hera@example.com", "com.example",
-                "hera@example.com"};
-// Submit the query and get a Cursor object back.
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.READ_CALENDAR}, MY_CAL_REQ);
-        }
-        cur = cr.query(uri, EVENT_PROJECTION, selection, null, null);
-
-        while (cur.moveToNext()) {
-
-            long calID = 0;
-            String displayName = null;
-            String accountName = null;
-            String ownerName = null;
-
-            // Get the field values
-            calID = cur.getLong(0);
-            displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
-            accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
-            ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
-
-            cur.close();
-
-            // Do something with the values...
-
-            TextView tv1 = new TextView(this);
-            tv1.setText(displayName);
-            layout.addView(tv1);*/
+            l_managedCursor.close();
+        } else {
 
         }
+    }
 
     private static final String DATE_TIME_FORMAT = "yyyy MMM dd, HH:mm:ss";
+
     public static String getDateTimeStr(int p_delay_min) {
         Calendar cal = Calendar.getInstance();
         SimpleDateFormat sdf = new SimpleDateFormat(DATE_TIME_FORMAT);
@@ -197,10 +154,12 @@ public class ViewCalendars extends FragmentActivity {
             return sdf.format(l_time);
         }
     }
+
     public static String getDateTimeStr(String p_time_in_millis) {
         SimpleDateFormat sdf = new SimpleDateFormat(DATE_TIME_FORMAT);
         Date l_time = new Date(Long.parseLong(p_time_in_millis.trim()));
         return sdf.format(l_time);
     }
-    }
+
+}
 
